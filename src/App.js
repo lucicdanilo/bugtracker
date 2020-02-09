7f2435bcaa5f9e93de5a4e757d4acfa17ff5d528
Danilo Lučić <lucic.danilo94@gmail.com>
2020-02-10 00:26:53 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container-fluid">
+        <h1 className="title">Bug Tracker</h1>
+        <h4 className="subtitle">
+          If you want best software for tracking bugs, you are on the good
+          place!
+        </h4>
+        <div className="mainMenu">
+          <button type="button" className="btn btn-outline-danger loginButton">
+            Log in
+          </button>
+          <button type="button" className="btn btn-danger registerButton">
+            Register
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
