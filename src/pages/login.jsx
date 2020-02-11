3ea775445ff2086fb1cd51a9303c654676c76496
Danilo Lučić <lucic.danilo94@gmail.com>
2020-02-11 11:05:58 +0100

    getting data from forms

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,12 +1,35 @@
 import React from "react";
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: ""
+    };
+  }
+
+  handleChange = event => {
+    this.setState({ email: event.target.email });
+    this.setState({ password: event.target.password });
+  };
+
+  handleSubmit(event) {
+    event.preventDefault();
+    var loginData = {
+      email: event.target.email.value,
+      password: event.target.password.value
+    };
+
+    var loginDataJSON = JSON.stringify(loginData);
+    console.log(loginDataJSON);
+  }
   render() {
     document.getElementById("mainTitle").style.visibility = "hidden";
     document.getElementById("mainSubtitle").style.visibility = "hidden";
     return (
       <div className="loginComponent">
-        <form className="form_login">
+        <form className="form_login" onSubmit={this.handleSubmit}>
           <div class="form-group">
             <label for="exampleInputEmail1">Email address</label>
             <input
@@ -15,6 +38,8 @@ class Login extends React.Component {
               id="exampleInputEmail1"
               aria-describedby="emailHelp"
               placeholder="Enter email"
+              name="email"
+              onChange={this.handleChange}
             />
           </div>
           <div class="form-group">
@@ -24,6 +49,8 @@ class Login extends React.Component {
               class="form-control"
               id="exampleInputPassword1"
               placeholder="Password"
+              name="password"
+              onChange={this.handleChange}
             />
           </div>
           <button type="submit" class="btn btn-primary">
--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,12 +1,53 @@
 import React from "react";
 
 class Register extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstName: "",
+      lastName: "",
+      username: "",
+      email: "",
+      password: "",
+      confirmPassword: ""
+    };
+  }
+
+  handleChange = event => {
+    this.setState({ firstName: event.target.firstName });
+    this.setState({ lastName: event.target.lastName });
+    this.setState({ username: event.target.username });
+    this.setState({ email: event.target.email });
+    this.setState({ password: event.target.password });
+    this.setState({ confirmPassword: event.target.confirmPassword });
+  };
+
+  handleSubmit(event) {
+    event.preventDefault();
+    var registerData = {
+      firstName: event.target.firstName.value,
+      lastName: event.target.lastName.value,
+      username: event.target.username.value,
+      email: event.target.email.value,
+      password: event.target.password.value,
+      confirmPassword: event.target.confirmPassword.value
+    };
+
+    var registerDataJSON = JSON.stringify(registerData);
+    console.log(registerDataJSON);
+
+    if (registerData.password === registerData.confirmPassword) {
+      console.log("isto");
+    } else {
+      console.log("nije isto");
+    }
+  }
   render() {
     document.getElementById("mainTitle").style.visibility = "hidden";
     document.getElementById("mainSubtitle").style.visibility = "hidden";
     return (
       <div className="registerComponent">
-        <form className="form_register">
+        <form className="form_register" onSubmit={this.handleSubmit}>
           <div className="form-group">
             <div class="row">
               <div class="col">
@@ -14,6 +55,8 @@ class Register extends React.Component {
                   type="text"
                   class="form-control"
                   placeholder="First name"
+                  name="firstName"
+                  onChange={this.handleChange}
                 />
               </div>
               <div class="col">
@@ -21,6 +64,8 @@ class Register extends React.Component {
                   type="text"
                   class="form-control"
                   placeholder="Last name"
+                  name="lastName"
+                  onChange={this.handleChange}
                 />
               </div>
             </div>
@@ -30,7 +75,13 @@ class Register extends React.Component {
               Username
             </label>
             <div class="col-sm-10">
-              <input type="email" class="form-control" id="inputUsername" />
+              <input
+                type="text"
+                class="form-control"
+                id="inputUsername"
+                name="username"
+                onChange={this.handleChange}
+              />
             </div>
           </div>
           <div class="form-group row">
@@ -38,7 +89,13 @@ class Register extends React.Component {
               Email
             </label>
             <div class="col-sm-10">
-              <input type="email" class="form-control" id="inputEmail" />
+              <input
+                type="email"
+                class="form-control"
+                id="inputEmail"
+                name="email"
+                onChange={this.handleChange}
+              />
             </div>
           </div>
           <div class="form-group row">
@@ -46,7 +103,13 @@ class Register extends React.Component {
               Password
             </label>
             <div class="col-sm-10">
-              <input type="password" class="form-control" id="inputPassword" />
+              <input
+                type="password"
+                class="form-control"
+                id="inputPassword"
+                name="password"
+                onChange={this.handleChange}
+              />
             </div>
           </div>
           <div class="form-group row">
@@ -58,6 +121,8 @@ class Register extends React.Component {
                 type="password"
                 class="form-control"
                 id="inputPasswordConfirm"
+                name="confirmPassword"
+                onChange={this.handleChange}
               />
             </div>
           </div>
